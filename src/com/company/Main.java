dcc08cf642293935c2f7a05eb49e8c6d81cba58e
RLott12 <36046819+RLott12@users.noreply.github.com>
2019-03-22 12:58:41 -0400

    outline

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> usernames = new ArrayList<>();
+        ArrayList<String> passwords = new ArrayList<>();
+        while(true){
+            System.out.println("Would you like to create an account or login?");
+            String inp = reader.nextLine();
+            if(inp.equalsIgnoreCase("login")){
+                System.out.println("Enter username");
+                String username = reader.nextLine();
+                System.out.println("Enter password");
+                String password = reader.nextLine();
+                for(int i = usernames.size() -1; i >=0; i--){
+                    
+                }
+            }
+            else if((inp.equalsIgnoreCase("create"))|| (inp.equalsIgnoreCase("create account"))||(inp.equalsIgnoreCase("create an account"))){
+
+            }
+
+        }
     }
 }
